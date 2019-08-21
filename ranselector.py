1f58e2ce2aaad81662fb13d40de7ff8c1463c6e8
vladcocheci <vladcocheci@gmail.com>
2019-08-21 16:23:12 +0300

    added ranselector.py -code to extract coordinates from the web site of the National Archeological Record of Romania

---
+++ ranselector.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import re
+import pandas as pd
+import csv
+
+output_file_name = "repertoriul_cluj_coord.csv"
+
+### main function
+def main():
+    df = pd.read_csv("repertoriul_cluj.csv")
+    x_field, y_field = [], []
+
+    for link in df["link_harta"]:
+        try:
+            coord = get_coordinates(link)
+        except:
+            coord = "Null : Null"
+        xy = coord.split(" : ")
+        x = xy[0]
+        y = xy[1]
+        print(x + ": " + y)
+        x_field.append(x)
+        y_field.append(y)
+
+
+    df["x_field"] = x_field
+    df["y_field"] = y_field
+    df.to_csv(output_file_name, index = False)
+
+     
+
+def get_coordinates(link):
+    browser = webdriver.Firefox()
+    browser.get(link)
+    browser.maximize_window()
+
+    elem = browser.find_element_by_id("divHarta")
+
+    actions = ActionChains(browser)
+    actions.move_to_element(elem).perform()
+    actions.click()
+
+    coordinates = browser.find_element_by_class_name("leaflet-control-mouseposition").text
+
+    browser.quit()
+    return coordinates
+
+if __name__ == "__main__":
+    main()
